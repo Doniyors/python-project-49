6528eb3046bd9f5b71d29f2b77a5da106de12d7d
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-05-28 08:08:05 +0300

    all

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import random
+import prompt
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+
+
+def main():
+    numbers = []
+    index_to_replace = random.randint(0, 9)
+    len_of_list = random.randint(5, 10)
+    number_to_join = random.randint(2, 5)
+    random_number = random.randint(1, 10)
+    numbers.append(random_number)
+    for _ in range(len_of_list):
+        previous_number = numbers[-1]
+        new_number = previous_number + number_to_join
+        numbers.append(new_number)
+    i = numbers[index_to_replace]
+    numbers[index_to_replace] = '..'
+    print(numbers)
+    user_input1 = input("Введите ваш ответ: ")
+    if int(user_input1) == i:
+        print('Correct!')
+        return True
+    else:
+        print(user_input1 + ' is wrong answer ;(. Correct answer was ' + str(i) + ". Let's try again!")
+        return False
+
+
+if __name__ == '__main__':
+    welcome_user()
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('What number is missing in the progression?')
+
+    correct_answers = 0
+    total_questions = 3
+
+    while correct_answers < total_questions:
+        if main():
+            correct_answers += 1
+        else:
+            break
+
+    if correct_answers == total_questions:
+        print('Congratulations, ' + name + '!')
