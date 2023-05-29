929008658ff2eeecdd723b845e12e4b397d9c858
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-05-29 21:17:28 +0300

    Final version nmb 16

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -28,7 +28,7 @@ def game(name):
         print('Correct!')
         return True
     else:
-        print(user_input1 + ' is wrong answer ;(. Correct answer was ' + str(i) + ". Let's try again, " + name + '!')
+        print(f"{user_input1} is wrong answer ;(. Correct answer was {str(i)}. Let's try again, {name}!")
         return False
 
 
