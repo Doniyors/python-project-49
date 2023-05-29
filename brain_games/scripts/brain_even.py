75371a0f26c0423d757630272a154ecf2de3065c
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-05-29 10:39:04 +0300

    add Final version of games nomber 3

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -15,39 +15,39 @@ def main():
     question3 = 7
     print(f'Question: {question1}')
     user_input1 = input("Your answer: ")
-    result = check_answer1(user_input1, name)
+    result = check_answer1(user_input1)
     print(result)
     if 'wrong answer' in result:
         return                                                         
     print(f'Question: {question2}')
     user_input2 = input("Your answer: ")
-    result = check_answer2(user_input2, name)
+    result = check_answer2(user_input2)
     print(result)
     if 'wrong answer' in result:
         return
     print(f'Question: {question3}')
     user_input3 = input("Your answer: ")
-    result = check_answer3(user_input3, name)
+    result = check_answer3(user_input3)
     print(result)    
 
 
-def check_answer1(answer, name):
+def check_answer1(answer):
     if answer.lower() == "no":
-        return f'Congratulations, {name}'
+        return f'Correct!'
     elif answer.lower() == "yes":
         return f"'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again,"
 
 
-def check_answer2(answer, name):
+def check_answer2(answer):
     if answer.lower() == "yes":
-        return f'Congratulations, {name}'
+        return f'Correct!'
     elif answer.lower() == "no":
         return f"'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, {name}"
 
 
-def check_answer3(answer, name):
+def check_answer3(answer):
     if answer.lower() == "no":
-        return f'Congratulations, {name}'
+        return f'Correct!'
     elif answer.lower() == "yes":
         return f"'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {name}"
 
