ba31d1c805c389794d77c5e54d84bcfb3186d8cd
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-05-29 20:59:55 +0300

    Final version nmb 13

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -6,25 +6,33 @@ import prompt
 def welcome_user():
     print("Welcome to the Brain Games!")
 
+def find_gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 def main():
     welcome_user()
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
+    print('Find the greatest common divisor of given numbers.')
     for _ in range(3):
-        i = (random.randint(1, 100))
-        i1 = (random.randint(1, 100))
+        number1 = random.randint(1, 100)
+        number2 = random.randint(1, 100)
+        gcd = find_gcd(number1, number2)
+        #i = (random.randint(1, 100))
+        #i1 = (random.randint(1, 100))
         #   minimum = min(i, i1)
-        maximum = max(i, i1)
-        print('Find the greatest common divisor of given numbers.')
-        print(f'Question: {i} {i1}')
+        #   maximum = max(i, i1)
+        print(f'Question: {number1} {number2}')
         user_input1 = input("Your answer: ")
-        if int(user_input1) == maximum:
+        if int(user_input1) == gcd:
             print('Correct!')
         else:
-            print(f"'{user_input1}' is wrong answer ;(. Correct answer was '{str(maximum)}'. Let's try again, {name}!")
+            print(f"'{user_input1}' is wrong answer ;(. Correct answer was '{str(gcd)}'. Let's try again, {name}!")
             return
     print('Congratulations, ' + name + '!')
+
 
 
 if __name__ == '__main__':
