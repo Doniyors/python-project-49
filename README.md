1680af2df8c04b2c01e59bdf299dbe1abe94407e
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-05-29 09:13:02 +0300

    add Final version of games

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/57ecaadb4a2caeafc3bf/maintainability)](https://codeclimate.com/github/Doniyors/python-project-49/maintainability) 
 Ссылка на asciinema 1 игра(yes or no): https://asciinema.org/a/1c5FNx9gYpJgYvz9tf3BmU2TT
 Ссылка на asciinema 2 игра(калькулятор): https://asciinema.org/a/YUcVeICjCJp9IrIUGycFvQuew
-Ссылка на asciinema 3 игра:
+Ссылка на asciinema 3 игра(НДО): https://asciinema.org/a/9DgUWCCH7uFPxg1XsO2BrHu4j
+Ссылка на asciinema 4 игры(арифметическая прогрессия): https://asciinema.org/a/9lVgOPCWBzGGeCX8iWb6NPuFv
+Ссылка на asciinema 5 игры(простое число или нет): https://asciinema.org/a/rrmZY2526EH16lHYXcZVlfmd8
+
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def is_prime(n): # Проверяет является ли число простым или нет
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def generate_prime():
+        num = random.randint(2, 10)  # Генерируйте числа в нужном диапазоне
+        print(f'Question: {num}')
+        user_input1 = input("Your answer: ")
+        if is_prime(num) == True and user_input1.lower() == 'yes':
+            print('Correct!')
+        if is_prime(num) == False and user_input1.lower() == 'no':
+            print('Correct!')
+        if is_prime(num) == False and user_input1.lower() == 'yes':
+            print(user_input1 + " is wrong answer ;(. Correct answer was 'yes'.Let s try again!")
+            return False
+        if is_prime(num) == True and user_input1.lower() == 'no':
+            print(user_input1 + " is wrong answer ;(. Correct answer was 'yes'.Let s try again!")
+            return False
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    for_congratulations = 0
+    for_congratulations_num_2 = 3
+    for _ in range(3):
+        for_congratulations += 1
+        if generate_prime() == False:
+            break
+        else:
+            continue
+
+    if for_congratulations == for_congratulations_num_2:
+        print("Congratulations, " + name + '!')
+    
+if __name__ == '__main__':
+    main()
