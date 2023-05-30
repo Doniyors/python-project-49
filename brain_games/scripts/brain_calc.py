20880efbe0c17524bd601afbd7922164e1d70c20
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-05-30 16:03:07 +0300

    viersion with mistake

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -36,7 +36,5 @@ def main():
             print(f"'{str(user_input1)}' is wrong answer ;(. Correct answer was '{str(i2)}'. Let's try again, {name}!")
             return
     print('Congratulations, ' + name + '!')
-
-
 if __name__ == '__main__':
     main()
