17f7be816e3f11039accc53da4dd098d4fc38582
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-06-03 16:14:02 +0300

    final version of games nmb-1

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,31 +4,30 @@ import random
 from brain_games.cli import welcome_user
 
 
-def game(name):
-    welcome_user()
-    numbers = []
-    len_of_list = random.randint(5, 10)
-    index_to_replace = random.randint(0, len_of_list - 1)
-    number_to_join = random.randint(2, 5)
-    random_number = random.randint(1, 10)
-    numbers.append(random_number)
-    for _ in range(len_of_list):
-        previous_number = numbers[-1]
-        new_number = previous_number + number_to_join
-        numbers.append(new_number)
-    i = numbers[index_to_replace]  # Значение которое нужно угадать
-    numbers[index_to_replace] = '..'
-    text = 'Question:'
-    for num in numbers:
-        text += f' {str(num) }'
-    print(text)
-    i5 = input("Your answer: ")
-    if int(i5) == i:
+def is_prime(n):  # Проверяет является ли число простым или нет
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def generate_prime(name):
+    num = random.randint(2, 10)  # Генерируйте числа в нужном диапазоне
+    print(f'Question: {num}')
+    user_input1 = input("Your answer: ")
+    if is_prime(num) is True and user_input1.lower() == 'yes':
         print('Correct!')
-        return True
-    else:
-        print(f"""'{i5}' is wrong answer ;(. Correct answer was '{str(i)}'.
-Let's try again, {name}!""")
+    if is_prime(num) is False and user_input1.lower() == 'no':
+        print('Correct!')
+    if is_prime(num) is False and user_input1.lower() == 'yes':
+        print(f"'{user_input1}' is wrong answer ;(.")
+        print(f"Correct answer was 'yes'. Let's try again, {name}!")
+        return False
+    if is_prime(num) is True and user_input1.lower() == 'no':
+        print(f"'{user_input1}' is wrong answer ;(.")
+        print(f"Correct answer was 'yes'. Let's try again, {name}!")
         return False
 
 
@@ -36,18 +35,18 @@ def main():
     welcome_user()
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
-    print('What number is missing in the progression?')
-    correct_answers = 0
-    total_questions = 3
-
-    while correct_answers < total_questions:
-        if game(name):
-            correct_answers += 1
-        else:
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    for_congratulations = 0
+    for_congratulations_num_2 = 3
+    for _ in range(3):
+        if generate_prime(name) is False:
             break
+        else:
+            for_congratulations += 1
+            continue
 
-    if correct_answers == total_questions:
-        print('Congratulations, ' + name + '!')
+    if for_congratulations == for_congratulations_num_2:
+        print("Congratulations, " + name + '!')
 
 
 if __name__ == '__main__':
