93b7d9484b5ecfeed1d01f7fd6ba5bff801fbcc5
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-06-09 16:30:22 +0300

    one more version of games

--- brain_games/brain_file.py
+++ brain_games/brain_file.py
@@ -6,54 +6,28 @@ from brain_games.games.brain_progression import main as brain_progression_main
 from brain_games.games.brain_even import main as brain_even_main
 from brain_games.games.brain_gcd import main as brain_gcd_main
 
-
-def main_games():
-    brain_games_main()
-
-
-def main_calc():
-    # Код для запуска игры brain_calc
-    brain_calc_main()
-
-
-def main_prime():
-    # Код для запуска игры brain_prime
-    brain_prime_main()
+games = {
+    'brain-calc': brain_calc_main,
+    'brain-prime': brain_prime_main,
+    'brain-progression': brain_progression_main,
+    'brain-games': brain_games_main,
+    'brain-even': brain_even_main,
+    'brain-gcd': brain_gcd_main
+}
 
 
-def main_progression():
-    # Код для запуска игры brain_progression
-    brain_progression_main()
+def main():
+    if len(sys.argv) < 2:
+        print("Не указана игра.")
+        return
 
+    game_name = sys.argv[1]
 
-def main_even():
-    # Код для запуска игры brain_even
-    brain_even_main()
-
-
-def main_gcd():
-    # Код для запуска игры brain_gcd
-    brain_gcd_main()
+    if game_name in games:
+        games[game_name].run()
+    else:
+        print(f"Неверное имя игры: {game_name}")
 
 
 if __name__ == '__main__':
-    available_games = {
-        'brain-calc': main_calc,
-        'brain-prime': main_prime,
-        'brain-progression': main_progression,
-        'brain-even': main_even,
-        'brain-gcd': main_gcd
-    }
-
-    if len(sys.argv) < 2:
-        print("""Не указана игра.
-        Укажите одну из доступных игр: brain-calc, brain-prime,
-        brain-progression, brain-even, brain-gcd""")
-    else:
-        game_name = sys.argv[1]
-        if game_name in available_games:
-            available_games[game_name]()
-        else:
-            print("""Неверное имя игры.
-            Укажите одну из доступных игр: brain-calc, brain-prime,
-            brain-progression, brain-even, brain-gcd""")
+    main()
