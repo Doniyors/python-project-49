3d9d077dc71ab45797504dce4e484b8f9ba74990
Дониёр Шавкати <doniyorshavkati@gmail.com>
2023-06-16 14:51:21 +0300

    последняя версия № 3

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,18 +4,30 @@ import random
 QUESTION = 'What number is missing in the progression?'
 
 
-def right_answer_and_question():
-    numbers = []
+def random_function():
     len_of_list = random.randint(5, 10)
     index_to_replace = random.randint(0, len_of_list - 1)
     number_to_join = random.randint(2, 5)
     random_number = random.randint(1, 10)
+    return len_of_list, index_to_replace, number_to_join, random_number
+
+
+def right_answer_and_question():
+    _, index_to_replace, _, _ = random_function()
+    numbers = give_question1()
+    # Значение которое нужно угадать
+    answer1 = numbers[index_to_replace]
+    numbers[index_to_replace] = '..'
+    question1 = " ".join(str(num) for num in numbers)
+    return question1, str(answer1)
+
+
+def give_question1():
+    numbers = []
+    len_of_list, _, number_to_join, random_number = random_function()
     numbers.append(random_number)
     for _ in range(len_of_list):
         previous_number = numbers[-1]
         new_number = previous_number + number_to_join
         numbers.append(new_number)
-    answer1 = numbers[index_to_replace]  # Значение которое нужно угадать
-    numbers[index_to_replace] = '..'
-    question1 = " ".join(str(num) for num in numbers)
-    return question1, str(answer1)
+    return numbers
